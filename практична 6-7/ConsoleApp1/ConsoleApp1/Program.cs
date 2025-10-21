122bf324bfeb5b65903c80346a5ba16459a40cb9
Студент <student@rivne.office.com>
2025-10-21 11:06:00 +0300

    практична 6-7

---
+++ практична 6-7/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿class MathOperation
+{
+    public virtual float Calculate(float a, float b)
+    {
+        return 0;
+    }
+}
+class SumOperation : MathOperation
+{
+    public override float Calculate(float a, float b)
+    {
+        return a + b;
+    }
+}
+class MultiplyOperation : MathOperation
+{
+    public override float Calculate(float a, float b)
+    {
+        return a * b;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        MathOperation sum = new SumOperation();
+        MathOperation multiply = new MultiplyOperation();
+
+        Console.WriteLine(sum.Calculate(5, 3));
+        Console.WriteLine(multiply.Calculate(5, 3));
+    }
+}
