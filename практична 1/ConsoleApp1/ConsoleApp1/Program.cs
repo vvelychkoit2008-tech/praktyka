a6c416283382773f6e6566c7781a75e8d19ff893
Студент <student@rivne.office.com>
2025-10-21 08:55:23 +0300

    fsdF

---
+++ практична 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace speed
+{
+    internal class speed { }
+    public class auto
+    {
+        private class dvygun
+        {
+            public void start()
+            {
+                Console.WriteLine("пєрємога буде");
+            }
+        }
+        public void zapusk()
+        {
+            dvygun d = new dvygun();
+            d.start();
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            auto a = new auto();
+            a.zapusk();
+        }
+    }
+}
---
+++ пратична 2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿class auto
+{
+    private int zapal;
+    public string Model { get; set; }
+    public auto(string model, int kod)
+    {
+        Model = model;
+        zapal = kod;
+    }
+    public void ac()
+    {
+        Console.WriteLine(Model + " прискорюється");
+    }
+    public void slow()
+    {
+        Console.WriteLine(Model + " гальмує");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        auto a = new auto("Toyota", 1);
+        a.ac();
+        a.slow();
+    }
+}
