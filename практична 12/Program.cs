2bf088024c1948d95f841ccfcb320db4dda3fbfd
Студент <student@rivne.office.com>
2025-10-23 09:03:34 +0300

    практична 12

---
+++ практична 12/Program.cs
@@ -0,0 +1,25 @@
+﻿class Animal
+{
+    public string Name { get; set; }
+    public string Sound { get; set; }
+    public void Hello()
+    {
+        Console.WriteLine($"{Name} каже {Sound}");
+    }
+}
+class Dog : Animal
+{
+    public Dog()
+    {
+        Name = "собака";
+        Sound = "мяу";
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Dog d = new Dog();
+        d.Hello();
+    }
+}
