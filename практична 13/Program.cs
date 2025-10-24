41f2c783a4a86814fc961ea05cab24874ef8c56f
vvelychkoit2008-tech <v.velychko_it_2008@rkepk.edu.ua>
2025-10-24 09:53:16 +0300

    практична 13

---
+++ практична 13/Program.cs
@@ -0,0 +1,27 @@
+﻿class Vyraz
+{
+    public string Value { get; set; }
+    // мій любімий пустий конструктор
+    public Vyraz() { }
+    public Vyraz(string text)
+    {
+        Value = text; 
+    }
+}
+class LongVyraz : Vyraz
+{
+    public int Length { get; }
+    public LongVyraz(string text)
+    {
+        Value = text;
+        Length = text.Length;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        LongVyraz vyraz = new LongVyraz("ещкере");
+        Console.WriteLine($"Вираз: {vyraz.Value}, Довжина: {vyraz.Length}");
+    }
+}
