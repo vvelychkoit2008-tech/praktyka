6e0efcd9eff6600a93f377a8960c5fc363f72b08
vvelychkoit2008-tech <v.velychko_it_2008@rkepk.edu.ua>
2025-10-24 10:03:56 +0300

    практична 14

---
+++ практична 14/Program.cs
@@ -0,0 +1,20 @@
+﻿class Button
+{
+    private static int Count = 0;
+    public void Click()
+    {
+        Count++;
+        Console.WriteLine("К-сть кліків: " + Count);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Button b1 = new Button();
+        Button b2 = new Button();
+        b1.Click();
+        b2.Click();
+        b1.Click();
+    }
+}
