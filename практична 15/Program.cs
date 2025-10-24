e2ebc57c4093b73a5f5e09beff640a2363d4c6e7
vvelychkoit2008-tech <v.velychko_it_2008@rkepk.edu.ua>
2025-10-24 10:17:32 +0300

    практична 15

---
+++ практична 15/Program.cs
@@ -0,0 +1,24 @@
+﻿class Math
+{
+    public double ToPower(double number, int power)
+    {
+        double result = 1;
+        for (int i = 0; i < power; i++)
+        {
+            result *= number;
+        }
+        return result;
+    }
+    public double ToPower(double number)
+    {
+        return number * number;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Math math = new Math();
+        Console.WriteLine(math.ToPower(3, 4));
+    }
+}
