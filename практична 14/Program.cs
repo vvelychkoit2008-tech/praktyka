c0c7f746d10f284f3784aae1cfbb1b9f187fd088
vvelychkoit2008-tech <v.velychko_it_2008@rkepk.edu.ua>
2025-10-25 19:01:59 +0300

    дороблена практична 14

--- практична 14/Program.cs
+++ практична 14/Program.cs
@@ -1,18 +1,19 @@
 ﻿class Button
 {
     private static int Count = 0;
+    public string Name { get; set; }
     public void Click()
     {
         Count++;
-        Console.WriteLine("К-сть кліків: " + Count);
+        Console.WriteLine("У "+ Name + " к-сть кліків: " + Count);
     }
 }
 class Program
 {
     static void Main()
     {
-        Button b1 = new Button();
-        Button b2 = new Button();
+        Button b1 = new Button() { Name = "перша кнопочка"};
+        Button b2 = new Button() { Name = "друга кнопочка"};
         b1.Click();
         b2.Click();
         b1.Click();
