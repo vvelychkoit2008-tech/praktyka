512a3f5038fe7edbbf250793e8a53bcf86ba0a7f
vvelychkoit2008-tech <v.velychko_it_2008@rkepk.edu.ua>
2025-10-27 13:41:58 +0200

    Add files via upload

--- практична 14/Program.cs
+++ практична 14/Program.cs
@@ -1,21 +1,31 @@
 ﻿class Button
 {
-    private static int Count = 0;
+    private int Count = 0; // приватний лічильник для кожної кнопки
     public string Name { get; set; }
+
     public void Click()
     {
         Count++;
-        Console.WriteLine("У "+ Name + " к-сть кліків: " + Count);
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("У " + Name + " к-сть кліків: " + Count);
     }
 }
+
 class Program
 {
     static void Main()
     {
-        Button b1 = new Button() { Name = "перша кнопочка"};
-        Button b2 = new Button() { Name = "друга кнопочка"};
+        Button b1 = new Button() { Name = "перша кнопочка" };
+        Button b2 = new Button() { Name = "друга кнопочка" };
+
         b1.Click();
         b2.Click();
         b1.Click();
+
+        b1.Show(); // У перша кнопочка к-сть кліків: 2
+        b2.Show(); // У друга кнопочка к-сть кліків: 1
     }
 }
